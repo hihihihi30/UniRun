2bfc208e854b2e20f92c49afa0a6e85aefffc22f
hihihihi30 <dhtmdgn77@gmail.com>
2025-09-18 16:32:05 +0900

    20250918
    장애물 설치까지

--- Assets/01.Scripts/ScrollingObject.cs
+++ Assets/01.Scripts/ScrollingObject.cs
@@ -1,19 +1,17 @@
 using UnityEngine;
 
+// 게임 오브젝트를 계속 왼쪽으로 움직이는 스크립트
 public class ScrollingObject : MonoBehaviour
 {
-    public float speed = 10f; // 이동속도
+    public float speed = 10f; // 이동 속도
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        //초당 speed의 속도로 왼쪽으로 평행이동
-      transform.Translate(Vector3.left * speed * Time.deltaTime);  
+        // 게임 오버가 아니라면
+        if (!GameManager.instance.isGameover)
+        {
+            // 초당 speed의 속도로 왼쪽으로 평행이동
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }        
     }
 }
